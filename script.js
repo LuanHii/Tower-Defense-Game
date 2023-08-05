0b6924dd51c88263ce20c38fdfab83eea6056371
LuanHii <luanhiivitor@gmail.com>
2023-08-04 22:13:15 -0300

    defenders and resources added

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300;
+
 
 
 // mouse
@@ -69,17 +72,64 @@ function handleGameGrid(){ // desenhando cada celula de grid individualmente
 // balas
 
 // defensores
+class Defender { // criando molde de defensor
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false;
+        this.health = 100;
+        this.projectiles = [];
+        this.timer = 0;
+        }
+        draw(){ //desenhando junto com vida
+            ctx.fillStyle = "blue";
+            ctx.fillRect(this.x,this.y, this.width, this.height);
+            ctx.fillStyle = "gold";
+            ctx.font = "30px Arial"
+            ctx.fillText(Math.floor(this.health), this.x + 25,this.y + 25);
+        }
+}
+canvas.addEventListener('click', function(){ // adicionando ao clicar
+    const gridPositionX = mouse.x - (mouse.x % cellSize)
+    const gridPositionY = mouse.y - (mouse.y % cellSize)
+    if (gridPositionY < cellSize) return; // não deixando adicionar na barra de controle
+    for (let i = 0; i < defenders.length; i++){ // não deixando adicionar em cima de um já existente
+        if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY) 
+        return;
+    }
+    let defenderCost = 100;
+    if (numberOfResources >= defenderCost){ // adicionando na array caso tenha recurso suficiente
+        defenders.push(new Defender(gridPositionX, gridPositionY));
+        numberOfResources -= defenderCost //removendo recurso após conseguir colocar
+
+    }
+});
+
+function handleDefenders(){
+    for (let i = 0; i < defenders.length; i++){
+        defenders[i].draw();
+    }
+}
 
 // inimigos
 
 //  recursos
 
 // utilidades
+function handleGameStatus(){ // lidando com status do jogo
+    ctx.fillStyle = "gold";
+    ctx.font = "30px Arial";
+    ctx.fillText("Recursos: " + numberOfResources, 30,30); // texto e quantidade de recurso do jogador
+}
 function animate(){ 
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.fillStyle = "blue";  // preenchendo barra de controle com azul
     ctx.fillRect(0,0,controlsBar.width,controlsBar.height); 
     handleGameGrid();
+    handleDefenders();
+    handleGameStatus();
     requestAnimationFrame(animate);
 }
 
