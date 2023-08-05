78e9dac58bfe5f343eecb2f8cb29315b6fce6497
LuanHii <luanhiivitor@gmail.com>
2023-08-05 19:59:35 -0300

    enemy, colission, new font

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
 const defenders = [];
+const enemies = [];
+const enemyPositions = [];
+let enemiesInterval = 600;
 let numberOfResources = 300;
+let frame = 0;
+let gameOver = false;
 
 
 
@@ -87,7 +92,7 @@ class Defender { // criando molde de defensor
             ctx.fillStyle = "blue";
             ctx.fillRect(this.x,this.y, this.width, this.height);
             ctx.fillStyle = "gold";
-            ctx.font = "30px Arial"
+            ctx.font = "15px 'Press Start 2P'"
             ctx.fillText(Math.floor(this.health), this.x + 25,this.y + 25);
         }
 }
@@ -110,18 +115,73 @@ canvas.addEventListener('click', function(){ // adicionando ao clicar
 function handleDefenders(){
     for (let i = 0; i < defenders.length; i++){
         defenders[i].draw();
+        for (let j = 0; j < enemies.length; j++){
+            if (collision(defenders[i], enemies[j])){ // se colidir com inimigo tirar vida e reduzir velocidade
+                enemies[j].movement = 0;
+                defenders[i].health -= 0.2;
+            }
+            if (defenders[i].health <=0){
+                defenders.splice(i,1);
+                i--;
+                enemies[j].movement = enemies[j].speed;
+            }
+        }
     }
 }
 
 // inimigos
+class Enemy{
+    constructor(verticalPosition){
+        this.x = canvas.width;
+        this.y = verticalPosition;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.speed = Math.random() * 0.2 + 0.4;
+        this.movement = this.speed;
+        this.health = 100;
+        this.maxHealth = this.health;
+    }
+    update(){
+        this.x -= this.movement;
+    }
+    draw(){
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.fillStyle = "black";
+        ctx.font = "15px 'Press Start 2P'"
+        ctx.fillText(Math.floor(this.health), this.x + 25,this.y + 25);
+
+    }
+}
+
+function handleEnemies(){
+    for (let i = 0; i < enemies.length; i++){
+        enemies[i].update();
+        enemies[i].draw();
+        if (enemies[i].x < 0){
+            gameOver = true;
+        }
+    }
+    if (frame % enemiesInterval === 0){
+        let verticalPosition = Math.floor(Math.random() * 5 + 1) * cellSize;
+        enemies.push(new Enemy(verticalPosition))
+        enemyPositions.push(verticalPosition);
+        if (enemiesInterval > 120) enemiesInterval -= 50;
+    }
+}
 
 //  recursos
 
 // utilidades
 function handleGameStatus(){ // lidando com status do jogo
-    ctx.fillStyle = "gold";
-    ctx.font = "30px Arial";
+    ctx.fillStyle = "Press Start 2P";
+    ctx.font = "30px 'Press Start 2P'";
     ctx.fillText("Recursos: " + numberOfResources, 30,30); // texto e quantidade de recurso do jogador
+    if (gameOver){ // animando mensagem de game over na tela
+        ctx.fillStyle = 'black';
+        ctx.font = "60px 'Press Start 2P'";
+        ctx.fillText("Fim de jogo", 135,330);
+    }
 }
 function animate(){ 
     ctx.clearRect(0,0, canvas.width, canvas.height);
@@ -129,8 +189,10 @@ function animate(){
     ctx.fillRect(0,0,controlsBar.width,controlsBar.height); 
     handleGameGrid();
     handleDefenders();
+    handleEnemies();
     handleGameStatus();
-    requestAnimationFrame(animate);
+    frame++;
+    if (!gameOver) requestAnimationFrame(animate); // se não está com gameover anime.
 }
 
 animate();
